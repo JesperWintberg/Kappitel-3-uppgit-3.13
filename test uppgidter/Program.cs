d31523d067335c16f7cf1eaf390a94e6086cde46
BaoLanNguyen <89245290+BaoLanNguyen@users.noreply.github.com>
2022-09-23 11:03:20 +0200

    Add project files.

---
+++ test uppgidter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder > 19)
+            {
+                Console.WriteLine("Du får tyvär inte vara med");
+            }
+
+            else if (ålder < 16)
+            {
+                Console.WriteLine("Du får tyvär inte vara med");
+            }
+
+            else
+            {
+                Console.WriteLine("Du får vara med");  
+            }
+
+
+
+        }
+    }
+}
+
+
